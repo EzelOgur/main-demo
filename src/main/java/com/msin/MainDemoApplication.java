a756d5cde84c1ce8ba0f7a3866aea03fb4d9c28a
user <greti.cani@moveengineering-eu.com>
2021-02-02 23:12:18 +0100

    res

--- src/main/java/com/msin/MainDemoApplication.java
+++ src/main/java/com/msin/MainDemoApplication.java
@@ -2,13 +2,19 @@ package com.msin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainDemoApplication {
+public class MainDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MainDemoApplication.class, args);
     }
 
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainDemoApplication.class);
+    }
 }
